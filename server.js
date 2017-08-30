bdae8552e25afcb1aa61e24638f4a4e1c912b02f
Sean Chen <skypemaster007@gmail.com>
2017-08-30 14:01:32 -0700

    Change port to 3000

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/auth');
